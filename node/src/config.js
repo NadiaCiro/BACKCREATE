cc82c1abf1a3a186b754c35cfd99b93308ac534f
NadiaCiro <luciguci262006@gmail.com>
2023-11-28 15:07:58 -0500

    Conexion Azure

--- node/src/config.js
+++ node/src/config.js
@@ -1,14 +1,19 @@
-import {config} from "dotenv"
+//import 'dotenv/config.js'
+//require('dotenv').config();
+//import './loadEnv';
+//import express from 'express';
+//import 'dotenv/config'
+//dotenv.config()
+import { config } from 'dotenv';
+config();
 
-config()
 
-export const PORT = process.env.PORT || 3001/*PORT: Es el servidor */
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'compras_create'
-export const DB_PORT = process.env.DB_PORT || 3306/*Puerto de la base de datos */
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''/*Puerto de la base de datos */
-
+export const PORT = process.env.PORT // || 3001/*PORT: Es el servidor */
+export const DB_USER = process.env.USER || 'root'
+export const DB_HOST = process.env.HOST || 'localhost'
+export const DB_DATABASE = process.env.DATABASE //|| 'compras_create'
+export const DB_PORT = process.env.PORTDB || 3306/*Puerto de la base de datos */
+export const DB_PASSWORD = process.env.PASSWORD || ''/*Puerto de la base de datos */
 
 
 
--- node/src/index.js
+++ node/src/index.js
@@ -1,7 +1,10 @@
 /* Arranca todo el programa, llama a la app, se inicializa el servidor localhost */
-import app from './app.js'
+import app from './app.js';
+import {PORT,DB_DATABASE,DB_HOST} from './config.js'
 
-import {PORT} from './config.js'
+
+
+
 
 app.listen(PORT)
-    console.log('SERVER  Corriendo el servidor http://localhost:3001/', PORT)
+    console.log('SERVER  Corriendo el servidor', PORT)
