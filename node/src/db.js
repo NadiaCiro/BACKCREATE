6f31229b50867a4640583427472a201a95b86afc
NadiaCiro <luciguci262006@gmail.com>
2023-12-06 19:24:02 -0500

    cambios

--- node/src/db.js
+++ node/src/db.js
@@ -1,10 +1,13 @@
 import {createPool} from 'mysql2/promise'
 import { DB_HOST, DB_USER, DB_PORT, DB_DATABASE,DB_PASSWORD } from "./config.js";
 
-export const pool =  createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password:DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE
-})
+export const pool = createPool(
+    {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+        port: 3306,
+        ssl: {rejectUnauthorized: false,
+        ca:('./certificados/DigiCertGlobalRootCA.crt.pem') }
+    });
